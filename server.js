ed95407f23d53610ad1aa3e42ae626d21a9d62f4
jmontoyapsl <julian.montoyag@perficient.com>
2022-02-15 23:22:59 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,89 @@
+// Get our dependencies
+var express = require('express');
+var app = express();
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST || 'localhost',
+  user     : process.env.DB_USER || 'user',
+  password : process.env.DB_PASS || 'password',
+  database : process.env.DB_NAME || 'movie_db'
+});
+
+connection.connect();
+
+function getMovies(callback) {    
+       connection.query("SELECT * FROM movie_db.moviereview",
+            function (err, rows) {
+               callback(err, rows); 
+           }
+       );    
+}
+
+function getReviewers(callback) {
+  connection.query("SELECT * FROM movie_db.reviewer",
+            function (err, rows) {
+               callback(err, rows); 
+           }
+       );    
+}
+
+function getPublications(callback) {
+  connection.query("SELECT * FROM movie_db.publication",
+            function (err, rows) {
+               callback(err, rows); 
+           }
+       );    
+}
+
+//Testing endpoint
+/*app.get('/', function(req, res){
+  var response = [{response : 'hello'}, {code : '200'}]
+  res.json(response);
+})*/
+
+// Implement the movies API endpoint
+app.get('/movies', function(req, res){
+  getMovies(function (err, moviesResult){ 
+    //you might want to do something is err is not null...      
+    res.json(moviesResult);
+  });
+})
+
+app.get('/', function(req, res, next) {   
+    //now you can call the get-driver, passing a callback function
+   getMovies(function (err, moviesResult){ 
+       //you might want to do something is err is not null...      
+      res.json(moviesResult);
+
+   });
+});
+
+// Implement the reviewers API endpoint
+app.get('/reviewers', function(req, res){
+  getReviewers(function (err, reviewersResult){ 
+    //you might want to do something is err is not null...      
+   res.json(reviewersResult);
+  });
+})
+
+// Implement the publications API endpoint
+app.get('/publications', function(req, res){
+  getPublications(function (err, publicationsResult){ 
+    //you might want to do something is err is not null...      
+   res.json(publicationsResult);
+  });
+})
+
+// Implement the pending reviews API endpoint
+app.get('/pending', function(req, res){
+  var pending = [
+    {title : 'Superman: Homecoming', release: '2017', score: 10, reviewer: 'Chris Harris', publication: 'International Movie Critic'},
+    {title : 'Wonder Woman', release: '2017', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
+    {title : 'Doctor Strange', release : '2016', score: 7, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'}
+  ]
+  res.json(pending);
+})
+console.log("server listening through port: "+process.env.PORT);
+// Launch our API Server and have it listen on port 3000.
+app.listen(process.env.PORT || 3000);
+module.exports = app;
